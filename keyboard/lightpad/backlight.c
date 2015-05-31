cfe6ccfd70b3148fde9322481fccd6550d025576
Ralf Schmitt <ralf@bunkertor.net>
2015-05-31 16:29:09 +0200

    lightpad, support for lightpad keypad

--- keyboard/lightpad/backlight.c
+++ keyboard/lightpad/backlight.c
@@ -18,28 +18,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <avr/io.h>
 #include "backlight.h"
 
-/* Backlight pin configuration
- *
- * FN1       PB0 (low)
- * FN2       PB5 (low)
- * FN3       PB4 (low)
- * FN4       PD7 (low)
- * REAR      PD6 (high)
- * NUMPAD    PB2 (high)
- * NUMLOCK   PB1 (low)
- */
 void backlight_init_ports() {
     DDRB |= (1<<0) | (1<<1) | (1<<2) | (1<<4) | (1<<5);
     DDRD |= (1<<6) | (1<<7);
 
-    backlight_disable_numlock();
+    backlight_enable_fn1();
+    backlight_disable_fn2();
+    backlight_disable_fn3();
+    backlight_disable_fn4();
 }
 
 void backlight_set(uint8_t level) {
-    (level & BACKLIGHT_FN1) ? backlight_enable_fn1() : backlight_disable_fn1();
-    (level & BACKLIGHT_FN2) ? backlight_enable_fn2() : backlight_disable_fn2();
-    (level & BACKLIGHT_FN3) ? backlight_enable_fn3() : backlight_disable_fn3();
-    (level & BACKLIGHT_FN4) ? backlight_enable_fn4() : backlight_disable_fn4();
     (level & BACKLIGHT_NUMPAD) ? backlight_enable_numpad() : backlight_disable_numpad();
     (level & BACKLIGHT_REAR) ? backlight_enable_rear() : backlight_disable_rear();
 }
