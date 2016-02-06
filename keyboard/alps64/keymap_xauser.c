63468ae457cbfd2088792b98dabd679d883c1048
Ralf Schmitt <ralf@bunkertor.net>
2016-02-06 10:36:52 +0000

    alps64, keymap xauser

---
+++ keyboard/alps64/keymap_xauser.c
@@ -0,0 +1,53 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Layer 0: QWERTY */
+    KEYMAP_AEK(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC, \
+               TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS, \
+               FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,ENT,  \
+               LSFT,Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,   \
+               LCTL,LGUI,LALT,               SPC,                RALT,FN1, RCTL),
+    /* Layer 1 : Space fn */
+    KEYMAP_AEK(FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,      \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,  \
+               TRNS,TRNS,TRNS,               FN4,                TRNS,RGUI,TRNS),
+    /* Layer 2: Nav */
+    KEYMAP_AEK(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,UP,   \
+               TRNS,TRNS,TRNS,               TRNS,               LEFT,DOWN,RGHT),
+    /* Layer 3: Mouse */
+    KEYMAP_AEK(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN1,BTN2, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WH_D,WH_U,MS_U,  \
+               TRNS,FN1, TRNS,               TRNS,               MS_L,MS_D,MS_R),
+    /* Layer 4: Toggle */
+    KEYMAP_AEK(FN5, FN6, FN7, FN8,TRNS, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,   \
+               TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS),
+    /* Layer 5: Functions */
+    KEYMAP_AEK(SLEP,F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, PWR,  \
+               TRNS,MSEL,VOLU,MPLY,PSCR,SLCK,PAUS,HOME,INS, DEL, END, TRNS,TRNS,TRNS, \
+               CAPS,MPRV,VOLD,MNXT,TRNS,TRNS,LEFT,DOWN,UP,  RGHT,TRNS,TRNS,WSCH, \
+               TRNS,MUTE,TRNS,TRNS,TRNS,TRNS,FN9,PGDN,PGUP,TRNS,TRNS,TRNS,   \
+               TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS)
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+    [0]  = ACTION_LAYER_TAP_KEY(4, KC_CAPS),
+    [1]  = ACTION_LAYER_MOMENTARY(5),
+    [2]  = ACTION_MODS_KEY(MOD_RSFT, KC_GRV),
+    [3]  = ACTION_LAYER_TAP_KEY(4, KC_ESC),
+    [4]  = ACTION_LAYER_TAP_KEY(5, KC_SPACE),
+    [5]  = ACTION_LAYER_SET_CLEAR(0),
+    [6]  = ACTION_LAYER_TOGGLE(1),
+    [7]  = ACTION_LAYER_TOGGLE(2),
+    [8]  = ACTION_LAYER_TOGGLE(3),
+    [9] = ACTION_MODS_KEY(MOD_LSFT, KC_INS)
+};
