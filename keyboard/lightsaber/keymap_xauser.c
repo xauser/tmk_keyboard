IneffableTao/tmk_keyboard-1

lightsaber, support for lightsaber keyboard

keyboard/lightsaber/keymap_xauser.c
@@ -1,0 +1,67 @@
+#include "keymap_common.h"
+#include "backlight.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* layer 0: qwerty */
+  KEYMAP(\
+      ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, HOME,DEL, PSCR,SLCK,PAUS, \
+      GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,NLCK,PSLS,PAST,PMNS, \
+      TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,P7,  P8,  P9,  PPLS, \
+      FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,INS, ENT, P4,  P5,  P6,  NO,   \
+      LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,NO,  RSFT,P1,  P2,  P3,  PENT, \
+      LCTL,LGUI,LALT,               SPC,                NO,  FN1, RGUI,RCTL,P0,  NO,  COMM,NO),  \
+  /* layer 1: space fn */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               FN4,                NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 2: gaming */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,UP,  TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,  TRNS,LEFT,DOWN,RGHT,TRNS, \
+      TRNS,NO,  TRNS,               TRNS,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 3: first function layer */
+  KEYMAP(\
+      TRNS,FN5, FN6, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN7, FN8, FN9, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WBAK,WH_D,WH_U,WFWD,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,ACL0,ACL1,ACL2,TRNS,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,TRNS,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               BTN1,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 4: second function layer */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,Y,   P,   PSCR,SLCK,PAUS,HOME,INS, DEL, END, VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS, \
+      CAPS,TRNS,TRNS,TRNS,TRNS,TRNS,LEFT,DOWN,UP,  RGHT,MPRV,MNXT,TRNS,MPLY,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     SLEP,TRNS,TRNS,TRNS,TRNS,FN10,PGDN,PGUP,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               TRNS,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO)
+};
+
+enum LayerId {
+  LAYER_QWERTY,
+  LAYER_SPACEFN,
+  LAYER_GAMING,
+  LAYER_FUNCTIONS_FIRST,
+  LAYER_FUNCTIONS_SECOND
+};
+
+const action_t fn_actions[] PROGMEM = {
+  [0]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_CAPS),
+  [1]  = ACTION_LAYER_MODS(LAYER_FUNCTIONS_SECOND, MOD_RALT),
+  [2]  = ACTION_MODS_KEY(MOD_RSFT, KC_GRV),
+  [3]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_ESC),
+  [4]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_SECOND, KC_SPACE),
+  [5]  = ACTION_BACKLIGHT_LEVEL(BACKLIGHT_SWITCH),
+  [6]  = ACTION_BACKLIGHT_LEVEL(BACKLIGHT_PCB),
+  [7]  = ACTION_DEFAULT_LAYER_SET(LAYER_QWERTY),
+  [8]  = ACTION_DEFAULT_LAYER_SET(LAYER_SPACEFN),
+  [9]  = ACTION_LAYER_TOGGLE(LAYER_GAMING),
+  [10] = ACTION_MODS_KEY(MOD_LSFT, KC_INS)
+};

xauser/tmk_keyboard

keyboard/lightsaber/keymap_xauser.c
@@ -1,0 +1,77 @@
+#include "keymap_common.h"
+#include "backlight.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* layer 0: qwerty */
+  KEYMAP(\
+      ESC, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, HOME,DEL, PSCR,SLCK,PAUS, \
+      GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,NLCK,PSLS,PAST,PMNS, \
+      TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,P7,  P8,  P9,  PPLS, \
+      FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,INS, ENT, P4,  P5,  P6,  NO,   \
+      LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,NO,  RSFT,P1,  P2,  P3,  PENT, \
+      LCTL,LGUI,LALT,               SPC,                NO,  FN1, RGUI,RCTL,P0,  NO,  COMM,NO),  \
+  /* layer 1: space fn */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               FN4,                NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 2: gaming */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,NO,  TRNS,               TRNS,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 3: nav */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,UP,  TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,  TRNS,LEFT,DOWN,RGHT,TRNS, \
+      TRNS,TRNS,TRNS,               TRNS,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 4: first function layer */
+  KEYMAP(\
+      TRNS,FN5, FN6, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      FN7, FN8, FN9, FN10,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WBAK,WH_D,WH_U,WFWD,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,ACL0,ACL1,ACL2,TRNS,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,TRNS,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               BTN1,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO),  \
+  /* layer 5: second function layer */
+  KEYMAP(\
+      TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      GRV, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,Y,   P,   PSCR,SLCK,PAUS,HOME,INS, DEL, END, VOLD,VOLU,MUTE,TRNS,TRNS,TRNS,TRNS, \
+      CAPS,M,   TRNS,TRNS,TRNS,TRNS,LEFT,DOWN,UP,  RGHT,MPRV,MNXT,TRNS,MPLY,TRNS,TRNS,TRNS,NO,   \
+      TRNS,     SLEP,TRNS,CALC,TRNS,WFAV,FN11,PGDN,PGUP,TRNS,TRNS,NO,  TRNS,TRNS,TRNS,TRNS,TRNS, \
+      TRNS,TRNS,TRNS,               TRNS,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,NO)
+};
+
+enum LayerId {
+  LAYER_QWERTY,
+  LAYER_SPACEFN,
+  LAYER_GAMING,
+  LAYER_NAV,
+  LAYER_FUNCTIONS_FIRST,
+  LAYER_FUNCTIONS_SECOND
+};
+
+const action_t fn_actions[] PROGMEM = {
+  [0]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_CAPS),
+  [1]  = ACTION_LAYER_MODS(LAYER_FUNCTIONS_SECOND, MOD_RALT),
+  [2]  = ACTION_MODS_KEY(MOD_RSFT, KC_GRV),
+  [3]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_ESC),
+  [4]  = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_SECOND, KC_SPACE),
+  [5]  = ACTION_BACKLIGHT_LEVEL(BACKLIGHT_SWITCH),
+  [6]  = ACTION_BACKLIGHT_LEVEL(BACKLIGHT_PCB),
+  [7]  = ACTION_DEFAULT_LAYER_SET(LAYER_QWERTY),
+  [8]  = ACTION_DEFAULT_LAYER_SET(LAYER_SPACEFN),
+  [9]  = ACTION_LAYER_TOGGLE(LAYER_GAMING),
+  [10] = ACTION_LAYER_TOGGLE(LAYER_NAV),
+  [11] = ACTION_MODS_KEY(MOD_LSFT, KC_INS)
+};