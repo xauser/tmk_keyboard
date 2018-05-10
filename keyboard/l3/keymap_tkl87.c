b6081b14daf63ce2a7b118e5ebd5f6a5525475e5
Ralf Schmitt <ralf@bunkertor.net>
2018-05-10 19:44:20 +0200

    l3, support for l3 based keyboards

---
+++ keyboard/l3/keymap_tkl87.c
@@ -0,0 +1,14 @@
+#include "keymap_common.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // layer 0: qwerty
+  KEYMAP_TKL87(
+      ESC,      F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,         PSCR,SLCK,PAUS,
+      GRV, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, NO,  BSPC,   INS, HOME,PGUP,
+      TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,        DEL, END, PGDN,
+      CAPS,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,NO,  ENT,
+      LSFT,NO,  Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,RSFT,                  UP,
+      LCTL,LGUI,LALT,               SPC,           NO,  LALT,RGUI,RCTL,             LEFT,DOWN,RGHT),
+};
+
+const action_t fn_actions[] PROGMEM = {};
