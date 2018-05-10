IneffableTao/tmk_keyboard-1

fc660c, added keymap xauser

keyboard/fc660c/keymap_xauser.c
@@ -1,0 +1,66 @@
+#include "fc660c.h"
+#include "hook.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* layer 0: qwerty */
+  KMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS,
+       TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
+       FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
+       LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,UP,
+       LCTL,LGUI,LALT,               SPC,                FN1, RGUI,RCTL,LEFT,DOWN,RGHT),
+  /* layer 1: space fn */
+  KMAP(FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     PGUP,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     PGDN,
+       FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               FN4,                TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 2: gaming */
+  KMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,FN5, TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 3: first function layer */
+  KMAP(FN6, FN7, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WBAK,WH_D,WH_U,WFWD,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,ACL0,ACL1,ACL2,TRNS,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               BTN1,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 4: second function layer */
+  KMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,     TRNS,
+       TRNS,TRNS,Y,   P,   PSCR,SLCK,PAUS,HOME,INS, DEL, END, VOLD,VOLU,MUTE,     TRNS,
+       CAPS,TRNS,TRNS,TRNS,WSCH,TRNS,LEFT,DOWN,UP,  RGHT,MPRV,MNXT,     MPLY,
+       TRNS,     SLEP,TRNS,CALC,TRNS,WFAV,FN9, PGDN,PGUP,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS)
+};
+
+enum LayerId {
+  LAYER_QWERTY,
+  LAYER_SPACEFN,
+  LAYER_GAMING,
+  LAYER_FUNCTIONS_FIRST,
+  LAYER_FUNCTIONS_SECOND
+};
+
+const action_t fn_actions[] PROGMEM = {
+  [0] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_CAPS),
+  [1] = ACTION_LAYER_MODS(LAYER_FUNCTIONS_SECOND, MOD_RALT),
+  [2] = ACTION_MODS_KEY(MOD_RSFT, KC_GRV),
+  [3] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_ESC),
+  [4] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_SECOND, KC_SPACE),
+  [5] = ACTION_LAYER_MOMENTARY(LAYER_FUNCTIONS_SECOND),
+  [6] = ACTION_DEFAULT_LAYER_SET(LAYER_QWERTY),
+  [7] = ACTION_DEFAULT_LAYER_SET(LAYER_SPACEFN),
+  [8] = ACTION_LAYER_TOGGLE(LAYER_GAMING),
+  [9] = ACTION_MODS_KEY(MOD_LSFT, KC_INS)
+};
+
+void hook_layer_change(uint32_t layer_state)
+{
+  layer_state & (1L<<2) ? (PORTB &= ~(1<<6)) : (PORTB |= (1<<6));
+}
+
+void hook_default_layer_change(uint32_t default_layer_state)
+{
+  default_layer_state & (1L<<1) ? (PORTB &= ~(1<<5)) : (PORTB |= (1<<5));
+}

xauser/tmk_keyboard

keyboard/fc660c/keymap_xauser.c
@@ -1,0 +1,66 @@
+#include "fc660c.h"
+#include "hook.h"
+
+const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* layer 0: qwerty */
+  KMAP(ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS,
+       TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL,
+       FN0, A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,
+       LSFT,     Z,   X,   C,   V,   B,   N,   M,   COMM,DOT, SLSH,     RSFT,UP,
+       LCTL,LGUI,LALT,               SPC,                FN1, RGUI,RCTL,LEFT,DOWN,RGHT),
+  /* layer 1: space fn */
+  KMAP(FN2, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     PGUP,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     PGDN,
+       FN3, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               FN4,                TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 2: gaming */
+  KMAP(TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,FN5, TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 3: first function layer */
+  KMAP(FN6, FN7, FN8, TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,WBAK,WH_D,WH_U,WFWD,TRNS,TRNS,TRNS,TRNS,     TRNS,
+       TRNS,ACL0,ACL1,ACL2,TRNS,TRNS,MS_L,MS_D,MS_U,MS_R,TRNS,TRNS,     TRNS,
+       TRNS,     TRNS,TRNS,TRNS,TRNS,TRNS,BTN3,BTN2,TRNS,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               BTN1,               NO,  TRNS,TRNS,TRNS,TRNS,TRNS),
+  /* layer 4: second function layer */
+  KMAP(GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12, TRNS,     TRNS,
+       TRNS,TRNS,Y,   P,   PSCR,SLCK,PAUS,HOME,INS, DEL, END, VOLD,VOLU,MUTE,     TRNS,
+       CAPS,M,   TRNS,TRNS,WSCH,TRNS,LEFT,DOWN,UP,  RGHT,MPRV,MNXT,     MPLY,
+       TRNS,     SLEP,TRNS,CALC,TRNS,WFAV,FN9, PGDN,PGUP,TRNS,TRNS,     TRNS,TRNS,
+       TRNS,TRNS,TRNS,               TRNS,               TRNS,TRNS,TRNS,TRNS,TRNS,TRNS)
+};
+
+enum LayerId {
+  LAYER_QWERTY,
+  LAYER_SPACEFN,
+  LAYER_GAMING,
+  LAYER_FUNCTIONS_FIRST,
+  LAYER_FUNCTIONS_SECOND
+};
+
+const action_t fn_actions[] PROGMEM = {
+  [0] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_CAPS),
+  [1] = ACTION_LAYER_MODS(LAYER_FUNCTIONS_SECOND, MOD_RALT),
+  [2] = ACTION_MODS_KEY(MOD_RSFT, KC_GRV),
+  [3] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_FIRST, KC_ESC),
+  [4] = ACTION_LAYER_TAP_KEY(LAYER_FUNCTIONS_SECOND, KC_SPACE),
+  [5] = ACTION_LAYER_MOMENTARY(LAYER_FUNCTIONS_SECOND),
+  [6] = ACTION_DEFAULT_LAYER_SET(LAYER_QWERTY),
+  [7] = ACTION_DEFAULT_LAYER_SET(LAYER_SPACEFN),
+  [8] = ACTION_LAYER_TOGGLE(LAYER_GAMING),
+  [9] = ACTION_MODS_KEY(MOD_LSFT, KC_INS)
+};
+
+void hook_layer_change(uint32_t layer_state)
+{
+  layer_state & (1L<<2) ? (PORTB &= ~(1<<6)) : (PORTB |= (1<<6));
+}
+
+void hook_default_layer_change(uint32_t default_layer_state)
+{
+  default_layer_state & (1L<<1) ? (PORTB &= ~(1<<5)) : (PORTB |= (1<<5));
+}